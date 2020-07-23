5973f6f2b516f410ae73624cde25086db76effbb
Marianna Patrusova <sumeragi.tanako@gmail.com>
2020-07-23 18:36:56 +0300

    3 - EntityBuilder & PathParser

--- domain/src/main/java/com/itechart/contacts/domain/entity/EntityBuilder.java
+++ domain/src/main/java/com/itechart/contacts/domain/entity/EntityBuilder.java
@@ -3,9 +3,11 @@ package com.itechart.contacts.domain.entity;
 import com.itechart.contacts.domain.entity.impl.*;
 import com.itechart.contacts.domain.exception.DaoException;
 import com.itechart.contacts.domain.util.DateConverter;
+import com.itechart.contacts.domain.util.PathParser;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import javax.servlet.http.HttpServletRequest;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -19,45 +21,16 @@ import java.time.LocalDate;
 public class EntityBuilder {
 
     private final static Logger LOGGER = LogManager.getLogger();
-    //column names:
-    private final static String ID_CONTACT = "id_contact";
-    private final static String CONTACT_NAME = "contact_name";
-    private final static String SURNAME = "surname";
-    private final static String PATRONYMIC = "patronymic";
-    private final static String BIRTHDAY = "birthday";
-    private final static String GENDER = "gender";
-    private final static String CITIZENSHIP = "citizenship";
-    private final static String STATUS = "family_status";
-    private final static String WEBSITE = "website";
-    private final static String EMAIL = "email";
-    private final static String WORK = "work_place";
-    private final static String ID_PHOTO = "id_photo";
-    private final static String COUNTRY = "country";
-    private final static String CITY = "city";
-    private final static String LOCATION = "address";
-    private final static String ZIPCODE = "zipcode";
-    private final static String DELETED = "deleted";
-    private final static String ID_ATTACHMENT = "id_attachment";
-    private final static String ATTACHMENT = "attachment_path";
-    private final static String NAME = "attachment_name";
-    private final static String DATE = "load_date";
-    private final static String COMMENTS = "comments";
-    private final static String CODE = "country_code";
-    private final static String OPERATOR = "operator_code";
-    private final static String NUMBER = "phone_number";
-    private final static String TYPE = "phone_type";
-    private final static String ID_PHONE = "id_phone";
-    private final static String PHOTO = "photo_path";
 
     public static AbstractEntity createAttachment(ResultSet resultSet) throws DaoException {
         AbstractEntity attachment;
         try {
-            long id = resultSet.getLong(ID_ATTACHMENT);
-            String path = resultSet.getString(ATTACHMENT);
-            String name = resultSet.getString(NAME);
-            LocalDate date = DateConverter.convertToLocalDate(resultSet.getDate(DATE));
-            String comments = resultSet.getString(COMMENTS);
-            long contactId = resultSet.getLong(ID_CONTACT);
+            long id = resultSet.getLong("id_attachment");
+            String path = PathParser.parse(resultSet.getString("attachment_path"));
+            String name = resultSet.getString("attachment_name");
+            LocalDate date = DateConverter.convertToLocalDate(resultSet.getDate("load_date"));
+            String comments = resultSet.getString("comments");
+            long contactId = resultSet.getLong("id_contact");
             attachment = new Attachment(id, path, name, date, comments, contactId);
         } catch (SQLException e) {
             LOGGER.log(Level.ERROR, "Cannot build attachment. Error has occurred. ", e);
@@ -66,26 +39,35 @@ public class EntityBuilder {
         return attachment;
     }
 
+    public static AbstractEntity createAttachment(HttpServletRequest request) {
+        String name = request.getParameter("attachment");
+        String path = "C:\\attachments\\" + name + "_" + new Timestamp(System.currentTimeMillis());
+        LocalDate date = LocalDate.now();
+        String comments = request.getParameter("comments");
+        long contactId = Long.parseLong(request.getParameter("contactId")); //сначала создать контакт, потом получить его айди, потом передать в аттачмент
+        return new Attachment(0L, path, name, date, comments, contactId);
+    }
+
     public static AbstractEntity createContact(ResultSet resultSet) throws DaoException {
         AbstractEntity contact;
         try {
-            long id = resultSet.getLong(ID_CONTACT);
-            String name = resultSet.getString(CONTACT_NAME);
-            String surname = resultSet.getString(SURNAME);
-            String patronymic = resultSet.getString(PATRONYMIC);
-            LocalDate birthday = DateConverter.convertToLocalDate(resultSet.getDate(BIRTHDAY));
-            Gender gender = Gender.getGender(resultSet.getString(GENDER));
-            String citizenship = resultSet.getString(CITIZENSHIP);
-            String familyStatus = resultSet.getString(STATUS);
-            String website = resultSet.getString(WEBSITE);
-            String email = resultSet.getString(EMAIL);
-            String work = resultSet.getString(WORK);
-            String country = resultSet.getString(COUNTRY);
-            String city = resultSet.getString(CITY);
-            String location = resultSet.getString(LOCATION);
-            String zipcode = resultSet.getString(ZIPCODE);
-            long photoId = resultSet.getLong(ID_PHOTO);
-            Timestamp deleted = resultSet.getTimestamp(DELETED);
+            long id = resultSet.getLong("id_contact");
+            String name = resultSet.getString("contact_name");
+            String surname = resultSet.getString("surname");
+            String patronymic = resultSet.getString("patronymic");
+            LocalDate birthday = DateConverter.convertToLocalDate(resultSet.getDate("birthday"));
+            Gender gender = Gender.getGender(resultSet.getString("gender"));
+            String citizenship = resultSet.getString("citizenship");
+            String familyStatus = resultSet.getString("family_status");
+            String website = resultSet.getString("website");
+            String email = resultSet.getString("email");
+            String work = resultSet.getString("work_place");
+            String country = resultSet.getString("country");
+            String city = resultSet.getString("city");
+            String location = resultSet.getString("address");
+            String zipcode = resultSet.getString("zipcode");
+            long photoId = resultSet.getLong("id_photo");
+            Timestamp deleted = resultSet.getTimestamp("deleted");
             contact = new Contact(id, name, surname, patronymic, birthday, gender,
                     citizenship, familyStatus, website, email, work,
                     country, city, location, zipcode, photoId, deleted);
@@ -96,16 +78,37 @@ public class EntityBuilder {
         return contact;
     }
 
+    public static AbstractEntity createContact(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String patronymic = request.getParameter("patronymic");
+        LocalDate birthday = LocalDate.parse(request.getParameter("birthday"));
+        Gender gender = Gender.getGender(request.getParameter("gender"));
+        String citizenship = request.getParameter("citizenship");
+        String familyStatus = request.getParameter("status");
+        String website = request.getParameter("website");
+        String email = request.getParameter("email");
+        String work = request.getParameter("work");
+        String country = request.getParameter("country");
+        String city = request.getParameter("city");
+        String location = request.getParameter("location");
+        String zipcode = request.getParameter("zipcode");
+        long photoId = Long.parseLong(request.getParameter("photoId"));
+        return new Contact(0L, name, surname, patronymic, birthday, gender,
+                citizenship, familyStatus, website, email, work,
+                country, city, location, zipcode, photoId, null);
+    }
+
     public static AbstractEntity createPhone(ResultSet resultSet) throws DaoException {
         AbstractEntity phone;
         try {
-            long id = resultSet.getLong(ID_PHONE);
-            String countryCode = resultSet.getString(CODE);
-            String operatorCode = resultSet.getString(OPERATOR);
-            String number = resultSet.getString(NUMBER);
-            String comments = resultSet.getString(COMMENTS);
-            PhoneType type = PhoneType.getPhoneType(resultSet.getString(TYPE));
-            long contactId = resultSet.getLong(ID_CONTACT);
+            long id = resultSet.getLong("id_phone");
+            String countryCode = resultSet.getString("country_code");
+            String operatorCode = resultSet.getString("operator_code");
+            String number = resultSet.getString("phone_number");
+            String comments = resultSet.getString("comments");
+            PhoneType type = PhoneType.getPhoneType(resultSet.getString("phone_type"));
+            long contactId = resultSet.getLong("id_contact");
             phone = new Phone(id, countryCode, operatorCode, number, type, comments, contactId);
         } catch (SQLException e) {
             LOGGER.log(Level.ERROR, "Cannot build attachment. Error has occurred. ", e);
@@ -114,11 +117,21 @@ public class EntityBuilder {
         return phone;
     }
 
+    public static AbstractEntity createPhone(HttpServletRequest request) {
+        String countryCode = request.getParameter("p_code");
+        String operatorCode = request.getParameter("p_operator");
+        String number = request.getParameter("number");
+        String comments = request.getParameter("p_comments");
+        PhoneType type = PhoneType.getPhoneType(request.getParameter("p_type"));
+        long contactId = Long.parseLong(request.getParameter("contactId")); //сначала создать контакт, потом получить его айди, потом передать в телефон
+        return new Phone(0L, countryCode, operatorCode, number, type, comments, contactId);
+    }
+
     public static AbstractEntity createPhoto(ResultSet resultSet) throws DaoException {
         AbstractEntity photo;
         try {
-            long id = resultSet.getLong(ID_PHOTO);
-            String path = resultSet.getString(PHOTO);
+            long id = resultSet.getLong("id_photo");
+            String path = PathParser.parse(resultSet.getString("photo_path"));
             photo = new Photo(id, path);
         } catch (SQLException e) {
             LOGGER.log(Level.ERROR, "Cannot build attachment. Error has occurred. ", e);
@@ -127,5 +140,9 @@ public class EntityBuilder {
         return photo;
     }
 
-}
+    public static AbstractEntity createPhoto(HttpServletRequest request) {
+        String path = request.getParameter("photo_path") + "_" + new Timestamp(System.currentTimeMillis());
+        return new Photo(0L, path);
+    }
 
+}
---
+++ domain/src/main/java/com/itechart/contacts/domain/util/PathParser.java
@@ -0,0 +1,18 @@
+package com.itechart.contacts.domain.util;
+
+/**
+ * Class for returning clear attachment path (without timestamp adds)
+ * @author Marianna Patrusova
+ * @version 1.0
+ */
+public class PathParser {
+
+    private PathParser() {}
+
+    public static String parse(String path) {
+        String timestamp = path.substring(path.lastIndexOf("_"));
+        path = path.replace(timestamp, "");
+        return path;
+    }
+
+}
