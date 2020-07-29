d69f9935a75afe3548b6224464b8f2bbcffd4430
Marianna Patrusova <sumeragi.tanako@gmail.com>
2020-07-29 23:19:37 +0300

    15 - js - load photo without db saving

--- view/src/main/webapp/js/popup-photo.js
+++ view/src/main/webapp/js/popup-photo.js
@@ -1,7 +1,8 @@
-//всплывающее окошко при загрузке фото:
+//обработка действий по загрузке пользовательского фото//
 
 'use strict';
 
+//вызов всплывающего окошка для загрузки фото
 var popupPhoto = document.querySelector('#messagePhoto');
 var photo = document.querySelector('.popupPhoto');
 var closePhoto = document.querySelectorAll('.close');
@@ -21,3 +22,32 @@ window.onclick = function (event) {
         popupPhoto.style.display = "none";
     }
 }
+
+//загрузка фото по умолчанию, если его нет
+var contactPhoto = document.querySelector('#searchImage');
+var checkedSrc = contactPhoto.getAttribute('src');
+
+//предпросмотр выбранного фото без сохранения на сервер
+var photoSrc;
+function readURL() {
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        $(reader).on('load', function (e) {
+            photoSrc = e.target.result;
+            $('#searchImage').attr('src', photoSrc);
+        });
+        reader.readAsDataURL(this.files[0]);
+    }
+}
+
+$("#photo").change(readURL);
+
+//сброс выбора к первоначальному фото
+var cancel = document.querySelector("#photoButton");
+cancel.addEventListener('click', function () {
+    contactPhoto.src = checkedSrc;
+    document.querySelector('#photo').value = '';
+    popupPhoto.style.display = "none";
+})
+
+
